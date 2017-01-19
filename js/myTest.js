ba75e31b01d344d1fd7a5e5c22eca0390aa59775
nxg13 <nicholas.glass@greenwichschools.org>
2017-01-18 21:43:40 -0500

    Add files via upload

--- js/myTest.js
+++ js/myTest.js
@@ -216,7 +216,7 @@ var buttonClicked = function() {
     questionCounter++;
     
     //MAKE TEST EASIER TO TRY OUT BY MAKING >= # SMALLER (full test is questionArray.length) !!!!!!!!!!!  
-    if (questionCounter >= questionArray.length) {
+    if (questionCounter >= 3) {
         //end the test here
         //display the score report, send scores to data base, etc.
         collectData();
